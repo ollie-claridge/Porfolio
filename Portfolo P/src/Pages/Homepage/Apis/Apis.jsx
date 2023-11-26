bb2cec9491dcb095747b5939b900792f56ad4658
ollie-claridge <ollie.claridge@gmail.com>
2023-11-26 22:03:50 +0000

    API Page

--- Portfolo P/src/Pages/Homepage/Apis/Apis.jsx
+++ Portfolo P/src/Pages/Homepage/Apis/Apis.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Apis.css'
+import FS from '../../../assets/FakestoreHP.png'
+
 
 function Apis() {
   return (
     <div className="api-page">
         <h1>Showcase of my API use</h1>
+        <div className="FS-example">
+    <img src = {FS} className="SHexample" alt="Fakestore Example" />
+    <div className="example-info">
+          <p>Fakesore Page</p>
+          <p>Using an API from open API</p>
+    </div>
+    </div>
     </div>
   )
 }
--- Portfolo P/src/Pages/Homepage/Homepage.jsx
+++ Portfolo P/src/Pages/Homepage/Homepage.jsx
@@ -17,21 +17,8 @@ function Homepage() {
       <h2 className='about-title'>About me</h2>
       <p>blah blah blah</p>
     </div>
-    <div className="mywork">
-      <h2>Some of examples of my work</h2>
-    </div>
-    <div className="example1">
-    <img src = {FS} className="SHexample" alt="Fakestore Example" />
-    <div className="example-info">
-          <p>Fakesore Page</p>
-          <p>Using an API from open API</p>
-    </div>
-    </div>
+
     <div className="example2">
-    <div className="example-info">
-        <p>UniLife Page</p>
-        <p>Another API</p>
-    </div>
       {/* <img src = {UL} className="SHexample"/> */}
     </div>
     <div className="example3">
