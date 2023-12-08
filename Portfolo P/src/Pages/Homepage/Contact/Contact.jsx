697938880f3e881aebd08cd332ab0423fe666c02
ollie-claridge <ollie.claridge@gmail.com>
2023-12-08 18:17:16 +0000

    Contact page started

--- Portfolo P/src/Pages/Homepage/Contact/Contact.jsx
+++ Portfolo P/src/Pages/Homepage/Contact/Contact.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './Contact.css'
+import { GrGithub } from "react-icons/gr";
+import { FaLinkedin } from "react-icons/fa";
+import { CiMail } from "react-icons/ci";
 
 function Contact() {
   return (
-    <div>Contact Information</div>
+    <div className='contact-page'>
+      <h1 className='contact-title'>Contacts</h1>
+      <div className='my-links'>
+        <div className="myLinks">
+          <p><GrGithub /> GITHUB</p>
+          <a>https://github.com/ollie-claridge</a>
+        </div>
+        <div className="myLinks">
+          <p><FaLinkedin /> LINKED IN</p>
+          <a>https://www.linkedin.com/in/oliver-claridge-3b5840296/</a>
+        </div>
+        <div className="myLinks">
+          <p><CiMail /> Email</p>
+          <p>ollie.claridge@gmail.com</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
