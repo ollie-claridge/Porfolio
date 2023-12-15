c0eec008d65e450cdd263b2cfc96a4c629cb5b07
ollie-claridge <ollie.claridge@gmail.com>
2023-12-15 18:47:50 +0000

    colors changed up

--- Portfolo P/src/Pages/Homepage/Homepage.jsx
+++ Portfolo P/src/Pages/Homepage/Homepage.jsx
@@ -13,30 +13,20 @@ function Homepage() {
     </div>
     
     <div className="ps">
-      <h2 className='about-title'>About me</h2>
+      <div className="about">
+      <h2 >About me</h2>
       <p>I have been learning to code while fitting it around my full-time job. I have completed a bootcamp course with Mimo-Dev this 
         year. Without a degree, it can be a challange finding work in this field but with the work I put in and how keen I am to learn
         whatever the popular language is necerssry for that situation.</p>
+      </div>
+   <div className="portrait">
+     <h2>(insert-protrait)</h2>
     </div>
 
-    <div className="example2">
-      {/* <img src = {UL} className="SHexample"/> */}
     </div>
-    <div className="example3">
-    <div className="example-info">
 
-    </div>
-    </div>
-    <div className="example4">
-    <div className="example-info"> 
-
-    </div>
-    </div>
-    <div className="example5">
-    <div className="example-info"> 
-    </div>
-      
-    </div>
+ 
+    
     </div>
 
   )
