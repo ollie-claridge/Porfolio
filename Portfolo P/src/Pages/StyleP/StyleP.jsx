f2a2929fadc59d7fe5b5f4e5c259cc35662d7595
ollie-claridge <ollie.claridge@gmail.com>
2023-12-20 19:51:19 +0000

    Started styles

--- Portfolo P/src/Pages/StyleP/StyleP.jsx
+++ Portfolo P/src/Pages/StyleP/StyleP.jsx
@@ -3,7 +3,18 @@ import './StyleP.css'
 
 function StyleP() {
   return (
-    <div>Showcase Style Skills</div>
+    <div className='style-page'>
+      <h1 className='style-header'>Styles</h1>
+      <p>- Flexbox</p>
+      <div className="flex-gid-example">
+        <div className="This">This</div>
+        <div className="Was">Was</div>
+        <div className="Made">Made</div>
+        <div className="Using">Using</div>
+        <div className="Flex">Flex</div>
+        <div className="Grid">Grid</div>
+      </div>
+    </div>
   )
 }
 
